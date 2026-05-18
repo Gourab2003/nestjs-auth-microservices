69c2d2130188cd713e302cabc8de9162420161b6
Gourab2003 <roygourab005@gmail.com>
2026-05-18 17:26:17 +0530

    nodemailer for mail services

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
 import * as amqp from 'amqplib';
